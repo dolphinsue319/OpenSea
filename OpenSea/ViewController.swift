5e46a0ac223d38b436765031ada03f565e45f8a2
Kedia <howardsue319@gmail.com>
2023-01-16 15:13:36 +0800

    Initial Commit

---
+++ OpenSea/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OpenSea
+//
+//  Created by Kedia on 2023/1/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
