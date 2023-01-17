0d7ecc614bcfab9385104a9cec6ffc749c7a499b
Kedia <howardsue319@gmail.com>
2023-01-17 16:28:45 +0800

    uses list view succeed

--- OpenSea/SceneDelegate.swift
+++ OpenSea/SceneDelegate.swift
@@ -10,13 +10,19 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    var appCoordinator: KDAppCoordinator?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        let navigationCon = UINavigationController()
+        appCoordinator = KDAppCoordinator(navCon: navigationCon)
+        appCoordinator?.start()
+        window?.rootViewController = navigationCon
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
