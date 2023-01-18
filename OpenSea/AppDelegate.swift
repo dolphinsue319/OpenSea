81ed7852b086ef8f0524e8c7290e99aa73236a89
Kedia <howardsue319@gmail.com>
2023-01-18 12:40:00 +0800

    updates nav bar background color

--- OpenSea/AppDelegate.swift
+++ OpenSea/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow(frame: UIScreen.main.bounds)
         let navigationCon = UINavigationController()
+        navigationCon.navigationBar.backgroundColor = .white
         appCoordinator = KDAppCoordinator(navCon: navigationCon)
         appCoordinator?.start()
         window?.rootViewController = navigationCon
